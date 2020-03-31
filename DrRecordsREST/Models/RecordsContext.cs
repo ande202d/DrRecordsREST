6dce97296e3cf8a899a1b73ce59911ecf58bc679
jaco6002 <jaco6002@edu.easj.dk>
2020-03-31 10:05:24 +0200

    started making in memory database

---
+++ DrRecordsREST/Models/RecordsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrRecordsREST.Models
+{
+    public class RecordsContext : DbContext
+    {
+        public RecordsContext(DbContextOptions<RecordsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Record> list { get; set; }
+    }
+}
